ffd26dae2a8f0c1797c09b613e8297c47edd273c
Jonny <Rivendell2003@outlook.com>
2024-09-22 22:16:44 -0300

    V.1.1

--- app/src/main/java/com/example/sprintmodulo4AboutJonny/MainActivity.java
+++ app/src/main/java/com/example/sprintmodulo4AboutJonny/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sprintmodulo4AboutJonny;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -11,7 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button callButton = findViewById(R.id.call_button);
+        callButton.setOnClickListener(v -> {
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+            callIntent.setData(Uri.parse("tel:123456789")); // Cambia el número por el que desees
+            startActivity(callIntent);
+        });
         Button contactButton = findViewById(R.id.contact_button);
         contactButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, com.example.sprintmodulo4AboutJonny.ContactActivity.class);
